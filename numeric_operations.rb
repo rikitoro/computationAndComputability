fee21357e4575068e604be64ab53fd5eafac2f74
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-03-16 13:21:04 +0900

    add DECREMENT

--- numeric_operations.rb
+++ numeric_operations.rb
@@ -1,2 +1,7 @@
+require_relative 'numbers'
+require_relative 'pairs'
 
 INCREMENT = -> n { -> p { -> x { p[n[p][x]] } } }
+
+SLIDE     = -> p { PAIR[RIGHT[p]][INCREMENT[RIGHT[p]]] }
+DECREMENT = -> n { LEFT[n[SLIDE][PAIR[ZERO][ZERO]]] }
--- spec/numeric_operations_spec.rb
+++ spec/numeric_operations_spec.rb
@@ -1,11 +1,17 @@
 require 'rspec/given'
 require_relative '../numeric_operations'
-require_relative '../numbers'
 
 describe "numeric_operations" do
   describe "INCREMENT" do
     Then { to_integer(INCREMENT[ZERO]) == 1 }
     Then { to_integer(INCREMENT[ONE]) == 2 }
     Then { to_integer(INCREMENT[FIFTEEN]) == 16 }
+  end
+
+  describe "DECREMENT" do
+    Then { to_integer(DECREMENT[FIVE]) == 4 }
+    Then { to_integer(DECREMENT[FIFTEEN]) == 14 }
+    Then { to_integer(DECREMENT[HUNDRED]) == 99 }
+    Then { to_integer(DECREMENT[ZERO]) == 0 }
   end
 end
