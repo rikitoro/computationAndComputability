b8ff94c40f8f9c012f7f4f4238f8701a15a69b3a
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-03-17 12:22:13 +0900

    add MULTIPLES_OF

--- infinite_streams.rb
+++ infinite_streams.rb
@@ -3,3 +3,10 @@ require_relative 'lists'
 ZEROS = Z[-> f { UNSHIFT[f][ZERO] }]
 
 UPWARDS_OF = Z[-> f { -> n { UNSHIFT[-> x { f[INCREMENT[n]][x] }][n] } }]
+
+MULTIPLES_OF =
+  -> m {
+    Z[-> f { 
+      -> n { UNSHIFT[->x { f[ADD[m][n]][x] }][n] }
+    }][m]
+  }
--- spec/infinite_streams_spec.rb
+++ spec/infinite_streams_spec.rb
@@ -14,5 +14,14 @@ describe "UPWARDS_OF" do
   Then { to_array(UPWARDS_OF[ZERO], 5).map { |p| to_integer(p) } 
     [0, 1, 2, 3, 4] }
   Then { to_array(UPWARDS_OF[FIFTEEN], 10).map { |p| to_integer(p) } 
-    [15, 16, 17, 18, 19, 20, 21, 22, 23,24] }
+    [3, ] }
+end
+
+describe "MULTIPLES_OF" do
+  Then { to_array(MULTIPLES_OF[THREE], 10).map { |p| to_integer(p) } 
+    [3, 6, 9, 12, 15, 18, 21, 24, 27, 30] }  
+  Then { to_array(MAP[MULTIPLES_OF[THREE]][INCREMENT], 10).map { |p| to_integer(p) } 
+    [4, 7, 10, 13, 16, 19, 22, 25, 28, 31] }  
+  Then { to_array(MAP[MULTIPLES_OF[THREE]][MULTIPLY[TWO]], 10).map { |p| to_integer(p) } 
+    [6, 12, 18, 24, 30, 36, 41, 48, 54, 60] }    
 end
