43905a33270210d84d748d0e8bbea5d5cac87dc9
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-03-22 20:31:11 +0900

    add #replace

--- implementing_lc/spec/lc_spec.rb
+++ implementing_lc/spec/lc_spec.rb
@@ -43,3 +43,88 @@ describe "LCs" do
     "-> m { -> n { n[-> n { -> p { -> x { p[n[p][x]] } } }][m] } }" }
 end
 
+describe "#replace" do
+  context "replace LCVariable" do
+    subject(:subject) { LCVariable.new(:x) }
+    context "x [x/->y { y }]" do
+      When(:replaced) { subject.replace(:x, 
+        LCFunction.new(:y, LCVariable.new(:y))) }
+      Then { replaced.inspect == "-> y { y }" }
+    end
+    context "x [z/->y { y }]" do
+      When(:replaced) { subject.replace(:z, 
+        LCFunction.new(:y, LCVariable.new(:y))) }
+      Then { replaced.inspect == "x" } 
+    end
+  end
+
+  context "replace LCCall" do
+    subject(:subject) { 
+      LCCall.new(
+        LCCall.new(
+          LCCall.new(
+            LCVariable.new(:a),
+            LCVariable.new(:b)
+          ),
+          LCVariable.new(:c)
+        ),
+        LCVariable.new(:b)
+      ) 
+    }
+
+    context "a[b][c][b] [a/x]" do
+      When(:replaced) { subject.replace(:a, LCVariable.new(:x)) }
+      Then { replaced.inspect == "x[b][c][b]" }
+    end
+
+    context "a[b][c][b] [b/-> x { x }]" do
+      When(:replaced) {
+        subject.replace(:b,
+          LCFunction.new(:x, LCVariable.new(:x)))
+      }
+      Then { replaced.inspect == 'a[-> x { x }][c][-> x { x }]'}
+    end
+  end
+
+  context "replace LCFunction" do
+    subject(:subject) { 
+      LCFunction.new(:y,
+        LCCall.new(LCVariable.new(:x), LCVariable.new(:y))) 
+    }
+
+    context "-> y { x[y] } [x/z]" do
+      When(:replaced) { subject.replace(
+        :x, LCVariable.new(:z))
+      }
+      Then { replaced.inspect == "-> y { z[y] }" }      
+    end
+
+    context "-> y { x[y] } [y/z]" do
+      When(:replaced) { subject.replace(
+        :y, LCVariable.new(:z))
+      }
+      Then { replaced.inspect == "-> y { x[y] }" }            
+    end
+  end
+
+  context "replace complex expression x[y][-> y { y[x] }" do
+    subject(:subject) { 
+      LCCall.new(
+        LCCall.new(LCVariable.new(:x), LCVariable.new(:y)),
+        LCFunction.new(:y, 
+          LCCall.new(LCVariable.new(:y), LCVariable.new(:x))
+        )
+      )
+    }
+
+    context "[x/z]" do
+      When(:replaced) { subject.replace(:x, LCVariable.new(:z)) }      
+      Then { replaced.inspect == 'z[y][-> y { y[z] }]' }
+    end
+
+    context "[y/z]" do
+      When(:replaced) { subject.replace(:y, LCVariable.new(:z)) }      
+      Then { replaced.inspect == 'x[z][-> y { y[x] }]' }       
+     end 
+  end
+end
