05cdcf2087f7ff05da823569d6489bd347111735
Yoshiaki Rikitake <yoshiaki@sendai-nct.ac.jp>
2015-04-03 17:34:53 +0900

    add CyclicTagRule and CyclicTagRulebook

--- tag_systems/tag_system.rb
+++ tag_systems/tag_system.rb
@@ -8,6 +8,7 @@ class TagRule < Struct.new(:first_character, :append_characters)
   end
 end
 
+
 class TagRulebook < Struct.new(:deletion_number, :rules)
   def next_string(string)
     rule_for(string).follow(string).slice(deletion_number..-1)
@@ -19,6 +20,44 @@ class TagRulebook < Struct.new(:deletion_number, :rules)
 
   def applies_to?(string)
     !rule_for(string).nil? && string.length >= deletion_number
+  end
+end
+
+class CyclicTagRule < TagRule
+  FIRST_CHARACTER = '1'
+
+  def initialize(append_characters)
+    super(FIRST_CHARACTER, append_characters)
+  end
+
+  def inspect
+    "#<CyclicTagRule #{append_characters.inspect}>"
+  end
+end
+
+class CyclicTagRulebook < Struct.new(:rules)
+  DELETION_NUMBER = 1
+
+  def initialize(rules)
+    super(rules.cycle)
+  end
+
+  def applies_to?(string)
+    string.length >= DELETION_NUMBER
+  end
+
+  def next_string(string)
+    follow_next_rule(string).slice(DELETION_NUMBER..-1)
+  end
+
+  def follow_next_rule(string)
+    rule = rules.next
+
+    if rule.applies_to?(string)
+      rule.follow(string)
+    else
+      string
+    end    
   end
 end
 
